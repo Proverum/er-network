bb2875052d937f1224ea00807e9357dd867fc958
LuxsThor <lucas.thorbecke@bluewin.ch>
2020-03-25 22:00:48 +0100

    nicer run express endpoints setup from one script

--- er-network/startExpress.sh
+++ er-network/startExpress.sh
@@ -3,13 +3,15 @@
 # this script start all the express app REST endpoint for all the userRegistrations
 # remember to node killall in case of a restart
 
-node ./../../api/javascript/municipality/app.js
-& node ./../../api/javascript/municipality2/app.js
-& node ./../../api/javascript/municipality3/app.js
-& node ./../../api/javascript/municipality4/app.js
-& node ./../../api/javascript/confederation/app.js
-& node ./../../api/javascript/canton/app.js
-& node ./../../api/javascript/canton2/app.js
+node ./../api/javascript/municipality/app.js &
+node ./../api/javascript/municipality2/app.js &
+node ./../api/javascript/municipality3/app.js &
+node ./../api/javascript/municipality4/app.js &
+node ./../api/javascript/confederation/app.js &
+node ./../api/javascript/canton/app.js &
+node ./../api/javascript/canton2/app.js &
+node ./../api/javascript/sp/app.js &
+node ./../api/javascript/esp/app.js &
 
 
 
